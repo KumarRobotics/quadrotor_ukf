2a2b91fa8e362d33fcf5dfcc677bb4d8c7001876
Dinesh Thakur <tdinesh@seas.upenn.edu>
2020-04-21 15:48:02 -0400

    cleanup ukf node, convert to class

--- src/quadrotor_ukf_node.cpp
+++ src/quadrotor_ukf_node.cpp
@@ -1,26 +1,112 @@
 #include <iostream>
+
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 #include <nav_msgs/Odometry.h>
 #include <quadrotor_ukf/quadrotor_ukf.h>
 #include <quadrotor_ukf/vio_utils.h>
 
-// ROS
-static ros::Publisher pubUKF;
-//ros::Publisher pubBias;
-static QuadrotorUKF quadrotorUKF;
-static std::string frame_id;
+class QuadrotorUkfNode
+{
+public:
+  QuadrotorUkfNode(std::string ns = "");
 
-//arma::mat H_C_B = arma::eye<mat>(4,4);//Never use reshape
-Eigen::Matrix<double, 4, 4> H_C_B;
+private:
 
-void imu_callback(const sensor_msgs::Imu::ConstPtr& msg)
+  void imu_callback(const sensor_msgs::Imu::ConstPtr& msg);
+  void slam_callback(const nav_msgs::Odometry::ConstPtr& msg);
+
+  ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
+  ros::Subscriber subImu_, subSLAM_;
+  ros::Publisher pubUKF_;
+  //ros::Publisher pubBias;
+
+  QuadrotorUKF quadrotorUKF_;
+
+  std::string frame_id_;
+
+  //arma::mat H_C_B = arma::eye<mat>(4,4);//Never use reshape
+  Eigen::Matrix<double, 4, 4> H_C_B_;
+
+  int calLimit_;
+  int calCnt_;
+  Eigen::Matrix<double, 3, 1> average_g_;
+
+};
+
+QuadrotorUkfNode::QuadrotorUkfNode(std::string ns): nh_(ns), pnh_("~")
+{
+  H_C_B_(0,0) = 1;
+  H_C_B_(0,1) = 0;
+  H_C_B_(0,2) = 0;
+  H_C_B_(0,3) = 0;
+  H_C_B_(1,0) = 0;
+  H_C_B_(1,1) = -1;
+  H_C_B_(1,2) = 0;
+  H_C_B_(1,3) = 0;
+  H_C_B_(2,0) = 0;
+  H_C_B_(2,1) = 0;
+  H_C_B_(2,2) = -1;
+  H_C_B_(2,3) = 0.0;
+  H_C_B_(3,0) = 0;
+  H_C_B_(3,1) = 0;
+  H_C_B_(3,2) = 0;
+  H_C_B_(3,3) = 1;
+
+  calLimit_ = 100;
+  calCnt_   = 0;
+
+  // UKF Parameters and Noise
+  double alpha;
+  double beta;
+  double kappa;
+  double stdAcc[3]     = {0,0,0};
+  double stdW[3]       = {0,0,0};
+  double stdAccBias[3] = {0,0,0};
+
+  pnh_.param("frame_id", frame_id_, std::string("/world"));
+  pnh_.param("alpha", alpha, 0.4);
+  pnh_.param("beta" , beta , 2.0);
+  pnh_.param("kappa", kappa, 0.0);
+  pnh_.param("noise_std/process/acc/x", stdAcc[0], 0.2);
+  pnh_.param("noise_std/process/acc/y", stdAcc[1], 0.2);
+  pnh_.param("noise_std/process/acc/z", stdAcc[2], 0.2);
+  pnh_.param("noise_std/process/w/x", stdW[0], 0.1);
+  pnh_.param("noise_std/process/w/y", stdW[1], 0.1);
+  pnh_.param("noise_std/process/w/z", stdW[2], 0.1);
+  pnh_.param("noise_std/process/acc_bias/x", stdAccBias[0], 0.05);
+  pnh_.param("noise_std/process/acc_bias/y", stdAccBias[1], 0.05);
+  pnh_.param("noise_std/process/acc_bias/z", stdAccBias[2], 0.05);
+
+  // Fixed process noise
+  Eigen::Matrix<double, 9, 9> Rv;
+  Rv.setIdentity();// = eye<mat>(9,9);
+  Rv(0,0)   = stdAcc[0] * stdAcc[0];
+  Rv(1,1)   = stdAcc[1] * stdAcc[1];
+  Rv(2,2)   = stdAcc[2] * stdAcc[2];
+  Rv(3,3)   = stdW[0] * stdW[0];
+  Rv(4,4)   = stdW[1] * stdW[1];
+  Rv(5,5)   = stdW[2] * stdW[2];
+  Rv(6,6)   = stdAccBias[0] * stdAccBias[0];
+  Rv(7,7)   = stdAccBias[1] * stdAccBias[1];
+  Rv(8,8)   = stdAccBias[2] * stdAccBias[2];
+
+  // Initialize UKF
+  quadrotorUKF_.SetUKFParameters(alpha, beta, kappa);
+  quadrotorUKF_.SetImuCovariance(Rv);
+
+  subImu_  = pnh_.subscribe<sensor_msgs::Imu>("imu", 10, boost::bind(&QuadrotorUkfNode::imu_callback, this,_1));
+  subSLAM_ = pnh_.subscribe<nav_msgs::Odometry>("odom_slam", 10, boost::bind(&QuadrotorUkfNode::slam_callback, this, _1));
+
+  pubUKF_  = pnh_.advertise<nav_msgs::Odometry>("control_odom", 10);
+  //pubBias_ = pnh_.advertise<geometry_msgs::Vector3>("/imu_bias", 10);
+}
+
+void QuadrotorUkfNode::imu_callback(const sensor_msgs::Imu::ConstPtr& msg)
 {
   // Assemble control input, and calibration
-  static int calLimit = 100;
-  static int calCnt   = 0;
-  //static colvec ag = zeros<colvec>(3);
-  static Eigen::Matrix<double, 3, 1> ag;
+
   Eigen::Matrix<double, 6, 1> u;
   u(0,0) = msg->linear_acceleration.x;
   u(1,0) = -msg->linear_acceleration.y;
@@ -28,25 +114,27 @@ void imu_callback(const sensor_msgs::Imu::ConstPtr& msg)
   u(3,0) = msg->angular_velocity.x;
   u(4,0) = -msg->angular_velocity.y;
   u(5,0) = -msg->angular_velocity.z;
-  if (calCnt < calLimit)       // Calibration
+
+  if (calCnt_ < calLimit_)       // Calibration
   {
-    calCnt++;
-    ag += u.block(0,0,3,1);//rows(0,2);
+    calCnt_++;
+    average_g_ += u.block(0,0,3,1);//rows(0,2);
   }
-  else if (calCnt == calLimit) // Save gravity vector
+  else if (calCnt_ == calLimit_) // Save gravity vector
   {
-    calCnt++;
-    ag /= calLimit;
-    double g = ag.norm();//norm(ag,2);
-    quadrotorUKF.SetGravity(g);
+    calCnt_++;
+    average_g_ /= calLimit_;
+    double g = average_g_.norm(); //norm(ag,2);
+    quadrotorUKF_.SetGravity(g);
   }
-  else if (quadrotorUKF.ProcessUpdate(u, msg->header.stamp))  // Process Update
+  else if (quadrotorUKF_.ProcessUpdate(u, msg->header.stamp))  // Process Update
   {
     nav_msgs::Odometry odomUKF;
+
     // Publish odom
-    odomUKF.header.stamp = quadrotorUKF.GetStateTime();
-    odomUKF.header.frame_id = frame_id;
-    Eigen::Matrix<double, Eigen::Dynamic, 1> x = quadrotorUKF.GetState();
+    odomUKF.header.stamp = quadrotorUKF_.GetStateTime();
+    odomUKF.header.frame_id = frame_id_;
+    Eigen::Matrix<double, Eigen::Dynamic, 1> x = quadrotorUKF_.GetState();
     odomUKF.pose.pose.position.x = x(0,0);
     odomUKF.pose.pose.position.y = x(1,0);
     odomUKF.pose.pose.position.z = x(2,0);
@@ -61,26 +149,28 @@ void imu_callback(const sensor_msgs::Imu::ConstPtr& msg)
     odomUKF.twist.twist.angular.x = u(3,0);
     odomUKF.twist.twist.angular.y = u(4,0);
     odomUKF.twist.twist.angular.z = u(5,0);
-    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>  P = quadrotorUKF.GetStateCovariance();
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>  P = quadrotorUKF_.GetStateCovariance();
     for (int j = 0; j < 6; j++)
       for (int i = 0; i < 6; i++)
         odomUKF.pose.covariance[i+j*6] = P((i<3)?i:i+3 , (j<3)?j:j+3);
     for (int j = 0; j < 3; j++)
       for (int i = 0; i < 3; i++)
         odomUKF.twist.covariance[i+j*6] = P(i+3 , j+3);
-    pubUKF.publish(odomUKF);
-/*
+
+    pubUKF_.publish(odomUKF);
+
+    /*
     // Publish bias
     geometry_msgs::Vector3 bias;
     bias.x = x(9);
     bias.y = x(10);
     bias.z = x(11);
-    pubBias.publish(bias);
-*/
+    pubBias_.publish(bias);
+    */
   }
 }
 
-void slam_callback(const nav_msgs::Odometry::ConstPtr& msg)
+void QuadrotorUkfNode::slam_callback(const nav_msgs::Odometry::ConstPtr& msg)
 {
   // Get orientation
   Eigen::Matrix<double, 4, 1> q;
@@ -89,6 +179,7 @@ void slam_callback(const nav_msgs::Odometry::ConstPtr& msg)
   q(2,0) = msg->pose.pose.orientation.y;
   q(3,0) = msg->pose.pose.orientation.z;
   Eigen::Matrix<double, 3, 1> ypr = VIOUtil::R_to_ypr(VIOUtil::QuatToMat(q));
+
   // Assemble measurement
   Eigen::Matrix<double, 6, 1> z;
   z(0,0) = msg->pose.pose.position.x;
@@ -97,6 +188,7 @@ void slam_callback(const nav_msgs::Odometry::ConstPtr& msg)
   z(3,0) = ypr(0,0);
   z(4,0) = ypr(1,0);
   z(5,0) = ypr(2,0);
+
   // Assemble measurement covariance
   Eigen::Matrix<double, 6, 6> RnSLAM;
   RnSLAM.setZero();// = zeros<mat>(6,6);
@@ -123,104 +215,47 @@ void slam_callback(const nav_msgs::Odometry::ConstPtr& msg)
   //mat H_R_R0 = zeros<mat>(4,4);
   Eigen::Matrix<double, 4, 4> H_R_R0;
   H_R_R0.setIdentity();
-  H_R_R0 = H_C_B*H_C_C0*H_C_B.inverse();
+  H_R_R0 = H_C_B_*H_C_C0*H_C_B_.inverse();
+
   //Set the rotation
   Eigen::Matrix<double, 4, 1> q_R_R0 = VIOUtil::MatToQuat(H_R_R0.block(0, 0, 3, 3));
+
   // Assemble measurement
   Eigen::Matrix<double, 6, 1> z_new;
   z_new(0,0) = H_R_R0(0,3);
   z_new(1,0) = H_R_R0(1,3);
   z_new(2,0) = H_R_R0(2,3);
+
   //define the matrix to rotate in the original frame
   Eigen::Matrix<double, 3, 1> ypr_new = VIOUtil::R_to_ypr(VIOUtil::QuatToMat(q_R_R0));//R_to_ypr(H_R_R0.submat(0, 0, 2, 2));
   z_new(3,0) = ypr_new(0,0);
   z_new(4,0) = ypr_new(1,0);
   z_new(5,0) = ypr_new(2,0);
+
   //rotate the covariance
   Eigen::Matrix<double, 6, 6>  RnSLAM_new;
   RnSLAM_new.setZero();// = zeros<mat>(6,6);
   //RnSLAM_new.submat(0,0,2,2) = H_C_B.submat(0, 0, 2, 2)*RnSLAM.submat(0,0,2,2)*H_C_B.submat(0, 0, 2, 2).t();
   //RnSLAM_new.submat(3,3,5,5) = H_C_B.submat(0, 0, 2, 2)*RnSLAM.submat(3,3,5,5)*H_C_B.submat(0, 0, 2, 2).t();
-  RnSLAM_new.block(0,0,3,3) = H_C_B.block(0, 0, 3, 3)*RnSLAM.block(0,0,3,3)*H_C_B.block(0, 0, 3, 3).transpose();
-  RnSLAM_new.block(3,3,3,3) = H_C_B.block(0, 0, 3, 3)*RnSLAM.block(3,3,3,3)*H_C_B.block(0, 0, 3, 3).transpose();
-  
+  RnSLAM_new.block(0,0,3,3) = H_C_B_.block(0, 0, 3, 3)*RnSLAM.block(0,0,3,3)*H_C_B_.block(0, 0, 3, 3).transpose();
+  RnSLAM_new.block(3,3,3,3) = H_C_B_.block(0, 0, 3, 3)*RnSLAM.block(3,3,3,3)*H_C_B_.block(0, 0, 3, 3).transpose();
+
   // Measurement update
-  if (quadrotorUKF.isInitialized())
+  if (quadrotorUKF_.isInitialized())
   {
-    quadrotorUKF.MeasurementUpdateSLAM(z_new, RnSLAM_new, msg->header.stamp);
+    quadrotorUKF_.MeasurementUpdateSLAM(z_new, RnSLAM_new, msg->header.stamp);
   }
   else
   {
-    quadrotorUKF.SetInitPose(z, msg->header.stamp);
+    quadrotorUKF_.SetInitPose(z, msg->header.stamp);
   }
 }
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "quadrotor_ukf");
-  ros::NodeHandle n("~");
-  
-  
-  H_C_B (0,0) = 1;
-  H_C_B (0,1) = 0;
-  H_C_B (0,2) = 0;
-  H_C_B (0,3) = 0;
-  H_C_B (1,0) = 0;
-  H_C_B (1,1) = -1;
-  H_C_B (1,2) = 0;
-  H_C_B (1,3) = 0;
-  H_C_B (2,0) = 0;
-  H_C_B (2,1) = 0;
-  H_C_B (2,2) = -1;
-  H_C_B (2,3) = 0.0;
-  H_C_B (3,0) = 0;
-  H_C_B (3,1) = 0;
-  H_C_B (3,2) = 0;
-  H_C_B (3,3) = 1;
 
-  // UKF Parameters and Noise
-  double alpha = 0.0;
-  double beta  = 0.0;
-  double kappa = 0.0;
-  double stdAcc[3]     = {0,0,0};
-  double stdW[3]       = {0,0,0};
-  double stdAccBias[3] = {0,0,0};
-  n.param("frame_id", frame_id, std::string("/world"));
-  n.param("alpha", alpha, 0.4);
-  n.param("beta" , beta , 2.0);
-  n.param("kappa", kappa, 0.0);
-  n.param("noise_std/process/acc/x", stdAcc[0], 0.2);
-  n.param("noise_std/process/acc/y", stdAcc[1], 0.2);
-  n.param("noise_std/process/acc/z", stdAcc[2], 0.2);
-  n.param("noise_std/process/w/x", stdW[0], 0.1);
-  n.param("noise_std/process/w/y", stdW[1], 0.1);
-  n.param("noise_std/process/w/z", stdW[2], 0.1);
-  n.param("noise_std/process/acc_bias/x", stdAccBias[0], 0.05);
-  n.param("noise_std/process/acc_bias/y", stdAccBias[1], 0.05);
-  n.param("noise_std/process/acc_bias/z", stdAccBias[2], 0.05);
-
-  // Fixed process noise
-  Eigen::Matrix<double, 9, 9> Rv;
-  Rv.setIdentity();// = eye<mat>(9,9);
-  Rv(0,0)   = stdAcc[0] * stdAcc[0];
-  Rv(1,1)   = stdAcc[1] * stdAcc[1];
-  Rv(2,2)   = stdAcc[2] * stdAcc[2];
-  Rv(3,3)   = stdW[0] * stdW[0];
-  Rv(4,4)   = stdW[1] * stdW[1];
-  Rv(5,5)   = stdW[2] * stdW[2];
-  Rv(6,6)   = stdAccBias[0] * stdAccBias[0];
-  Rv(7,7)   = stdAccBias[1] * stdAccBias[1];
-  Rv(8,8)   = stdAccBias[2] * stdAccBias[2];
-
-  // Initialize UKF
-  quadrotorUKF.SetUKFParameters(alpha, beta, kappa);
-  quadrotorUKF.SetImuCovariance(Rv);
-
-  ros::Subscriber subImu  = n.subscribe("imu" ,      10, imu_callback, ros::TransportHints().tcpNoDelay());
-  ros::Subscriber subSLAM = n.subscribe("odom_slam", 10, slam_callback, ros::TransportHints().tcpNoDelay());
-  pubUKF  = n.advertise<nav_msgs::Odometry>("control_odom", 10);
-  //pubBias = n.advertise<geometry_msgs::Vector3>("/imu_bias", 10);
-
+  QuadrotorUkfNode ukf_node;
   ros::spin();
 
   return 0;
